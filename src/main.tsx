64f77276d3024d1b8a306cd775c53fee9c4b1847
simochee <lollipop@simochee.net>
2024-03-29 19:26:16 +0900

    ✨ setup tailwindcss v4

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
-console.log('application ready.')
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./main.css";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <StrictMode>
+    <p className="text-red-800 font-bold">Hello, World.</p>
+  </StrictMode>
+);
