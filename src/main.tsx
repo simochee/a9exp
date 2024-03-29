11d85d01340b91320ef1723eb603e881f3dc8669
simochee <lollipop@simochee.net>
2024-03-29 19:35:33 +0900

    👕 lint files

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 
-const root = createRoot(document.getElementById("root")!);
+const rootEl = document.getElementById("root");
 
-root.render(
-  <StrictMode>
-    <p className="text-red-800 font-bold">Hello, World.</p>
-  </StrictMode>
-);
+if (rootEl instanceof HTMLElement) {
+	const root = createRoot(rootEl);
+	root.render(
+		<StrictMode>
+			<p className="text-red-800 font-bold">Hello, World.</p>
+		</StrictMode>,
+	);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss()],
 });
